ff6895792a44b4a9f4f2fe0bf7476b6a65a0ac38
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-07-23 12:42:45 +0600

    oop

---
+++ Explore_OOP/proto_prototype.js
@@ -0,0 +1,21 @@
+function P1(a, b) {
+  this.a = a;
+  this.b = b;
+}
+
+function Obj2(a, b, c) {
+  P1.call(this, a, b);
+  this.c = c;
+}
+
+Obj2.prototype = Object.create(P1.prototype);
+
+function Obj3() {
+  Obj2.call(this, 10, 15, 20);
+}
+
+Obj3.prototype = Object.create(Obj2.prototype);
+
+// var obj1 = new Obj(1, 2);
+const obj2 = new Obj2(4, 5, 6);
+const obj3 = new Obj3();
